28349135b548fdd02680e01243692f3cce0ae404
jim4067 <jimmyimpulse2@gmail.com>
2020-11-24 22:49:28 +0300

    feat: reading user std::in using io library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Can you guess the number?");
+
+    println!("Okay, show me what you got...");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed:  {}", guess);
 }
